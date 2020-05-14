9c1250ac5a7b7718e1cd8d3b2c66f20afe27b8b5
Gaurav Gautam <gaurav.gautam@livspace.com>
2020-05-14 15:27:59 +0530

    feat(router): Modify the router-mfe component

--- src/components/mfe.js
+++ src/components/mfe.js
@@ -1,5 +1,40 @@
+const MFEBooter = {
+  name: 'mfe-booter',
+  props: {
+    mfe: {},
+    depth: {}
+  },
+  render (h) {
+    console.log('Booting mfe: ', this.mfe)
+    this.$nextTick(() => {
+      // @rohit send the cache back to the mfe here so it can get hydrated
+      this.mountmfe()
+    })
+    // @rohit cache the dom here
+    return h('div', { attrs: { 'mfe-name': this.mfe && this.mfe.name }})
+  },
+  methods: {
+    mountmfe () {
+      if (this.mfe) { //  TODO: Modify to check that this vm is mounted on host
+        let shadowroot = this.$el.shadowRoot
+        if (!shadowroot) {
+          shadowroot = this.$el.attachShadow({ mode: 'open' })
+        }
+        shadowroot.innerHTML = ''
+        const shodowhost = document.createElement('div')
+        shadowroot.appendChild(shodowhost)
+
+        this.mfe.boot(shodowhost, { mountpoint: shadowroot, router: this.$router, depth: this.depth }).then(() => {
+          this.$emit('bootfinished')
+        })
+      }
+    }
+  }
+}
+
 export default {
-  name: 'RouterMfe',
+  name: 'router-mfe',
+  functional: true,
   props: {
     name: {
       type: String,
@@ -13,63 +48,38 @@ export default {
       type: String
     }
   },
-  render (h) {
+  render (h, { props, children, parent, data }) {
     // used by devtools to display a router-view badge
     // data.routerView = true
     // const h = parent.$createElement
-    this.$vnode.data.routerView = true
-    const route = this.$parent.$route
+    data.routerView = true
+    const route = parent.$route
     const depth = 0
     const matched = route.matched[depth]
+
+    let vnode = h('div', { attrs: { 'mfe-router-outlet': true }}, 'No mfe was matched!')
+
     if (matched && matched.mfes) {
       const name = Object.keys(matched.mfes)[0]
       const mfe = matched && matched.mfes[name]
+
       if (name && mfe) {
         const subroutes = mfe.routes
-        this.$parent.$router.addRoutes([{
+        parent.$router.addRoutes([{
           name: route.name,
           path: route.path,
           children: subroutes
         }])
       }
+
+      vnode = h('div',
+        {
+          attrs: { 'mfe-router-outlet': true }
+        },
+        [
+          h(MFEBooter, { props: { mfe, depth: depth + 1 }})
+        ])
     }
-    this.$nextTick(() => {
-      this.boot()
-    })
-    return h('div', { ref: 'host', attrs: { 'mfe-router-outlet': true }})
-  },
-
-  methods: {
-    boot () {
-      const route = this.$parent.$route
-      const depth = 0
-      const matched = route.matched[depth]
-      if (matched && matched.mfes) {
-        const name = Object.keys(matched.mfes)[0]
-        const mfe = matched && matched.mfes[name]
-        if (!mfe.mfevm) {
-          if (this.useShadowDom) {
-            const host = this.$refs.host
-            const shadowroot = host.attachShadow({ mode: 'open' })
-
-            const shodowhost = document.createElement('div')
-
-            shadowroot.appendChild(shodowhost)
-            mfe.boot(shodowhost, { mountpoint: shadowroot, router: this.$parent.$router, depth: depth + 1 }).then(() => {
-              this.$emit('bootfinished')
-            })
-          } else {
-            const host = this.$refs.host
-            mfe.boot(host, { mountpoint: this.$refs.host, router: this.$parent.$router, depth: depth + 1 }).then(() => {
-              this.$emit('bootfinished')
-            })
-          }
-        }
-      }
-    }
-  },
-
-  mounted () {
-    this.boot()
+    return vnode
   }
 }
